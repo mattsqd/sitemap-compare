b5ee721f6be4c5aec7d4d1dc2e1013d0b9c13131
Matt Poole <mattsqd@users.noreply.github.com>
2025-04-10 10:37:23 -0400

    Remove generate report from after each test.

--- cypress/e2e/compare.cy.js
+++ cypress/e2e/compare.cy.js
@@ -91,10 +91,6 @@ describe(`Comparing sitemap URLs`, () => {
                     cy.compareSnapshot(compareOptions);
                 }
             });
-            after(() => {
-                // This causes an extra JSON report to be created if run via CLI.
-                cy.task('generateJsonReport');
-            })
         });
     });
 });
